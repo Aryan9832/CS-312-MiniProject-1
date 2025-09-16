f5a9f5f22acebc502f9a8664883ab59be154c750
Aryan Sharma <as6355@nau.edu>
2025-09-15 21:36:05 -0700

    ss

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const methodOverride = require('method-override');
 const path = require('path');
 
-const app = express();
+const app = express();  
 const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
